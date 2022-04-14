f8f755111c20b50d37b5b9f41cace1857c6ca8a7
Shivar Jadoonanan <sbshivar@gmail.com>
2022-04-14 10:31:52 -0400

    added support for historical data

--- main.js
+++ main.js
@@ -3,7 +3,10 @@ import express, { response } from 'express';
 
 const app = express();
 
-app.listen(8000, '10.0.0.240', () => console.log('listening on port 8000'));
+var apikey = '';
+var apikeyHistorical = '';
+
+app.listen(8000, '0.0.0.0', () => console.log('listening on port 8000'));
 app.use(express.static('public'));
 app.use(express.text());
 app.use((req, res, next) => {
@@ -17,9 +20,21 @@ app.post('/api/query', async(request, response) => {
     const data = request.body;
     console.log(data);
     let searchData = await fetch('https://api.openweathermap.org/data/2.5/weather?q='+ 
-    data +'&appid=61937ce3b5d91ccf517cf0cfc8bb10af');
+    data +'&appid=' + apikey);
     var obj = await searchData.json();
-    if(obj == null){
+    if(obj == null) {
+        response.sendStatus(404);
+    } else {
+        response.send(obj);
+    }
+})
+
+app.post('/api/historical', async(request, response) => {
+    const data = request.body;
+    console.log(data);
+    let searchData = await fetch('http://api.weatherbit.io/v2.0/history/daily' + data + '&key=' + apikeyHistorical);
+    var obj = await searchData.json();
+    if(obj == null) {
         response.sendStatus(404);
     } else {
         response.send(obj);
